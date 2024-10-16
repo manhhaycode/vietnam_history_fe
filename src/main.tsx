06d732ff67cd89762abd903cb2ce5ea743b62df4
sunny-yv <yenvy.sunny@gmail.com>
2024-10-16 13:10:36 +0700

    fix conflict

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { makeServer } from '../mocks/server.ts';
+// import { makeServer } from '../mocks/server.ts';
 
 const originalWarn = console.warn;
 console.warn = (...args) => {
