201bf21fe1dd913e21c7325b86fe6329ee11e7f2
manhhaycode <manhnvse173470@fpt.edu.vn>
2024-11-06 06:09:02 +0700

    Change data date

--- src/features/era/components/UpsertEraModal/UpsertEraModal.tsx
+++ src/features/era/components/UpsertEraModal/UpsertEraModal.tsx
@@ -1,90 +1,91 @@
 import {
-    Button,
-    Input,
-    Modal,
-    ModalBody,
-    ModalContent,
-    ModalHeader,
-    Select,
-    SelectItem,
-    Textarea,
-    useDisclosure,
-  } from '@nextui-org/react';
-  import { EEraStatus, IEra } from '@/features/era'; 
-  import { useForm } from 'react-hook-form';
-  import { useEffect } from 'react';
-  
-  export default function UpsertEraModal({
-    state,
-    data,
-    onSubmitForm,
-  }: {
-    state: ReturnType<typeof useDisclosure>;
-    data: IEra | null;
-    onSubmitForm?: (data: IEra, isEdit: boolean) => void;
-  }) {
-    const { handleSubmit, register, reset } = useForm<IEra>({
-      defaultValues: { status: EEraStatus.PENDING },
-    });
-  
-    const onSubmit = handleSubmit((dataSubmit) => {
-      if (onSubmitForm) onSubmitForm(dataSubmit, !!data);
-    });
-  
-    useEffect(() => {
-      if (state.isOpen) {
-        if (data) reset(data);
-        else setTimeout(() => reset(), 0);
-      }
-    }, [state.isOpen, data, reset]);
-  
-    return (
-      <Modal placement="top" size="xl" isOpen={state.isOpen} onOpenChange={state.onOpenChange}>
-        <ModalContent>
-          {() => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">
-                {data ? 'Cập nhật thời đại' : 'Tạo thời đại mới'}
-              </ModalHeader>
-              <ModalBody>
-                <form onSubmit={onSubmit}>
-                  <div className="grid grid-cols-1 gap-4">
-                    <Input label="Tên thời đại" {...register('name')} defaultValue={data?.name} />
-                    <Textarea label="Mô tả" {...register('description')} defaultValue={data?.description} />
-                    <Input label="Hình ảnh" {...register('thumbnail')} defaultValue={data?.thumbnail} />
-                    <Input
-                      type="datetime-local"
-                      label="Ngày bắt đầu"
-                      {...register('startDate')}
-                      defaultValue={data?.startDate}
-                    />
-                    <Input
-                      type="datetime-local"
-                      label="Ngày kết thúc"
-                      {...register('endDate')}
-                      defaultValue={data?.endDate}
-                    />
-                    <Select
-                      label="Trạng thái"
-                      {...register('status')}
-                      defaultSelectedKeys={[data?.status ?? EEraStatus.PENDING]}
-                    >
-                      {Object.values(EEraStatus).map((option) => (
-                        <SelectItem key={option} value={option}>
-                          {option}
-                        </SelectItem>
-                      ))}
-                    </Select>
-                    <Button radius="sm" color="primary" type="submit">
-                      {data ? 'Cập nhật' : 'Tạo mới'}
-                    </Button>
-                  </div>
-                </form>
-              </ModalBody>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
-    );
-  }
-  
+  Button,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  Select,
+  SelectItem,
+  Textarea,
+  useDisclosure,
+} from '@nextui-org/react';
+import { EEraStatus, IEra } from '@/features/era';
+import { useForm } from 'react-hook-form';
+import { useEffect } from 'react';
+import dayjs from 'dayjs';
+
+export default function UpsertEraModal({
+  state,
+  data,
+  onSubmitForm,
+}: {
+  state: ReturnType<typeof useDisclosure>;
+  data: IEra | null;
+  onSubmitForm?: (data: IEra, isEdit: boolean) => void;
+}) {
+  const { handleSubmit, register, reset } = useForm<IEra>({
+    defaultValues: { status: EEraStatus.PENDING },
+  });
+
+  const onSubmit = handleSubmit((dataSubmit) => {
+    if (onSubmitForm)
+      onSubmitForm(
+        {
+          ...dataSubmit,
+          startDate: dayjs(dataSubmit.startDate).toISOString(),
+          endDate: dayjs(dataSubmit.endDate).toISOString(),
+        },
+        !!data,
+      );
+  });
+
+  useEffect(() => {
+    if (state.isOpen) {
+      if (data)
+        reset({
+          ...data,
+          startDate: dayjs(data.startDate).format('YYYY-MM-DD'),
+          endDate: dayjs(data.endDate).format('YYYY-MM-DD'),
+        });
+      else setTimeout(() => reset(), 0);
+    }
+  }, [state.isOpen, data, reset]);
+
+  return (
+    <Modal placement="top" size="xl" isOpen={state.isOpen} onOpenChange={state.onOpenChange}>
+      <ModalContent>
+        {() => (
+          <>
+            <ModalHeader className="flex flex-col gap-1">{data ? 'Cập nhật thời đại' : 'Tạo thời đại mới'}</ModalHeader>
+            <ModalBody>
+              <form onSubmit={onSubmit}>
+                <div className="grid grid-cols-1 gap-4">
+                  <Input label="Tên thời đại" {...register('name')} defaultValue={data?.name} />
+                  <Textarea label="Mô tả" {...register('description')} defaultValue={data?.description} />
+                  <Input label="Hình ảnh" {...register('thumbnail')} defaultValue={data?.thumbnail} />
+                  <Input label="Ngày bắt đầu" {...register('startDate')} defaultValue={data?.startDate} />
+                  <Input label="Ngày kết thúc" {...register('endDate')} />
+                  <Select
+                    label="Trạng thái"
+                    {...register('status')}
+                    defaultSelectedKeys={[data?.status ?? EEraStatus.PENDING]}
+                  >
+                    {Object.values(EEraStatus).map((option) => (
+                      <SelectItem key={option} value={option}>
+                        {option}
+                      </SelectItem>
+                    ))}
+                  </Select>
+                  <Button radius="sm" color="primary" type="submit">
+                    {data ? 'Cập nhật' : 'Tạo mới'}
+                  </Button>
+                </div>
+              </form>
+            </ModalBody>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
+  );
+}
--- src/layouts/AdminLayout/AdminLayout.tsx
+++ src/layouts/AdminLayout/AdminLayout.tsx
@@ -1,13 +1,20 @@
 import AdminNav from '@/features/admin/components/AdminNav';
+import { useAuthStore } from '@/libs/store';
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 
 export default function AdminLayout() {
+  const { user, setUser } = useAuthStore();
+  useEffect(() => {
+    if (user && user.roleId !== '999') {
+      setUser(null);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
   return (
     <div className="flex relative">
       <AdminNav />
-      <div className="flex-1 px-4 relative overflow-x-hidden">
-        <Outlet />
-      </div>
+      <div className="flex-1 px-4 relative overflow-x-hidden">{user?.roleId === '999' && <Outlet />}</div>
     </div>
   );
 }
