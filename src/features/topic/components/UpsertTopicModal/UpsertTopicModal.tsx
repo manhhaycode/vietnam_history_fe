5b38f9f6500a452e40d3cca219b416c3aaa01607
Nguyễn Phạm <nguyenppse161566@gmail.com>
2024-11-08 23:03:22 +0700

    Update Topic

--- src/features/topic/components/UpsertTopicModal/UpsertTopicModal.tsx
+++ src/features/topic/components/UpsertTopicModal/UpsertTopicModal.tsx
@@ -13,14 +13,20 @@ import {
 import { ETopicStatus, ITopic } from '@/features/topic';
 import { useForm } from 'react-hook-form';
 import { useEffect } from 'react';
+import { IEra } from '@/features/era';
+import { IEvent } from '@/features/event';
 export default function UpsertTopicModal({
   state,
   data,
   onSubmitForm,
+  eras = [],
+  events = [],
 }: {
   state: ReturnType<typeof useDisclosure>;
   data: ITopic | null;
   onSubmitForm?: (data: ITopic, isEdit: boolean) => void;
+  eras: { id: IEra['id']; name: string }[];
+  events: { id: IEvent['id']; name: string }[];
 }) {
   const { handleSubmit, register, reset } = useForm<ITopic>({ defaultValues: { status: ETopicStatus.PUBLISHED } });
 
@@ -50,6 +56,31 @@ export default function UpsertTopicModal({
                   <Input label="Tên chủ đề" {...register('name')} defaultValue={data?.name} />
                   <Textarea label="Mô tả" {...register('description')} defaultValue={data?.description} />
                   <Input label="Hình ảnh" {...register('thumbnail')} defaultValue={data?.thumbnail} />
+
+                  <Select
+                    label="Thời đại"
+                    {...register('eraId')}
+                    defaultSelectedKeys={data?.eraId ? [data.eraId] : undefined}
+                  >
+                    {eras.map((era) => (
+                      <SelectItem key={era.id} value={era.id}>
+                        {era.name}
+                      </SelectItem>
+                    ))}
+                  </Select>
+
+                  <Select
+                    label="Sự kiện"
+                    {...register('eventId')}
+                    defaultSelectedKeys={data?.eventId ? [data.eventId] : undefined}
+                  >
+                    {events.map((event) => (
+                      <SelectItem key={event.id} value={event.id}>
+                        {event.name}
+                      </SelectItem>
+                    ))}
+                  </Select>
+
                   <Select
                     label="Trạng thái"
                     {...register('status')}
